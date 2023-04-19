921b4cc16c3fce4d54e62252a8b12bbcec23c779
NinilinaO <128506535+NinilinaO@users.noreply.github.com>
2023-04-19 20:47:00 +0500

    task3

--- app/src/main/java/com/msaggik/fourthlessontrainticketsgroup/MainActivity.java
+++ app/src/main/java/com/msaggik/fourthlessontrainticketsgroup/MainActivity.java
@@ -7,17 +7,20 @@ import android.widget.TextView;
 
 import com.msaggik.fourthlessontrainticketsgroup.model.RailwayTicket;
 import com.msaggik.fourthlessontrainticketsgroup.model.RailwayTicketChild;
+import com.msaggik.fourthlessontrainticketsgroup.model.RailwayTicketOld;
 
 public class MainActivity extends AppCompatActivity {
 
     // создание объекта сущности взрослых билетов (стоимость билета, количество билетов)
-    RailwayTicket railwayTicket = new RailwayTicket(120, 2);
+    RailwayTicket railwayTicket = new RailwayTicket(70, 9);
     // создание объекта сущности детских билетов (стоимость билета, количество билетов, скидка)
-    RailwayTicket railwayTicketChild = new RailwayTicketChild(120, 14, 50);
+    RailwayTicket railwayTicketChild = new RailwayTicketChild(70, 11, 50);
+    RailwayTicket railwayTicketOld = new RailwayTicketOld(70, 5, 30);
 
     // создание полей для вывода на экран нужных значений
     private TextView railwayTicketOut; // поле вывода общей стоимости взрослых билетов
     private TextView railwayTicketChildOut; // поле вывода общей стоимости детских билетов
+    private TextView railwayTicketOldOut;
     private TextView railwayTicketTotalOut; // поле вывода общей стоимости всех билетов
 
     // вывод на экран полученных значений
@@ -29,7 +32,8 @@ public class MainActivity extends AppCompatActivity {
         // присваивание переменным активити элементов представления activity_main
         railwayTicketOut = findViewById(R.id.railwayTicketOut); // вывод информации стоимости всех взрослых билетов
         railwayTicketChildOut = findViewById(R.id.railwayTicketChildOut); // вывод информации стоимости всех детских билетов
-        railwayTicketTotalOut = findViewById(R.id.railwayTicketTotalOut); // вывод информации стоимости всех билетов
+        railwayTicketTotalOut = findViewById(R.id.railwayTicketTotalOut);
+        railwayTicketOldOut = findViewById(R.id.railwayTicketOldOut);
 
         // запонение экрана
         // 1) заполнение первого поля воспоьзовавшись методом подсчёта стоимости взрослых билетов ticketPriceAll()
@@ -37,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
         railwayTicketOut.setText(Float.toString(railwayTicket.ticketPriceAll()) + " монет");
         // 2) заполнение второго поля воспользовавшись переопределённым методом подсчёта стоимости детских билетов ticketPriceAll()
         railwayTicketChildOut.setText(Float.toString(railwayTicketChild.ticketPriceAll()) + " монет");
+
+        railwayTicketOldOut.setText(Float.toString(railwayTicketOld.ticketPriceAll()) + " монет");
         // 3) заполнение третьего поля стоимостью всех билетов
         railwayTicketTotalOut.setText(Float.toString(railwayTicket.ticketPriceAll() + railwayTicketChild.ticketPriceAll()) + " монет");
 
